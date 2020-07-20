8f209bceabe9762b39d9e7ed031ecbffe25c50bf
cpetillot <constance.petillot@hetic.net>
2020-07-20 14:13:40 +0200

    finishing feature collection

--- shop/assets/feature-collection.js
+++ shop/assets/feature-collection.js
@@ -6,6 +6,3 @@ $(form)
     var id = $(this).data("variant-id");
     $(form).find('input[name="id"]').val(id);
   });
-
-onclick =
-  "$(this).parent().parent().find(input[name='id']).val($(this).data('variant-id'))";
